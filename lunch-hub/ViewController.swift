e03f52b9e9d983096ed6448982fc16b5ffd25b8c
mitani-shimpei <shimpei1127@gmail.com>
2019-04-24 20:34:44 +0900

    create project

---
+++ lunch-hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lunch-hub
+//
+//  Created by shinpei mitani on 2019/04/24.
+//  Copyright © 2019 jp.co.tokyo-lunch-hub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lunch-hubTests/lunch_hubTests.swift
@@ -0,0 +1,34 @@
+//
+//  lunch_hubTests.swift
+//  lunch-hubTests
+//
+//  Created by shinpei mitani on 2019/04/24.
+//  Copyright © 2019 jp.co.tokyo-lunch-hub. All rights reserved.
+//
+
+import XCTest
+@testable import lunch_hub
+
+class lunch_hubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
