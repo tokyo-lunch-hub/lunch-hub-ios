f384d3c92932051c36e896312d31461c12d48339
atsushi130 <atsushi_main@outlook.jp>
2019-04-30 21:56:27 +0900

    implement: change host method

--- Data/Api/LunchHubApi/LunchHubApi.swift
+++ Data/Api/LunchHubApi/LunchHubApi.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2019 Tokyo Lunch Hub. All rights reserved.
 //
 
-public struct LunchHubApi {}
+public struct LunchHubApi {
+    
+    public static let shared = LunchHubApi()
+    private init() {}
+    
+    public static func changeHost(to apiHost: LunchHubApiHost) {
+        lunchHubApiHost = apiHost
+    }
+}
