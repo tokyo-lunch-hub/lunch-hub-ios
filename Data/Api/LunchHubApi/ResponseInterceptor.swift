dfca3f3993b166f1fe3cb3dd5afbe38c74f6f51d
atsushi130 <atsushi_main@outlook.jp>
2019-04-30 22:28:02 +0900

    implement: SuccessResponseInterceptor

--- Data/Api/LunchHubApi/ResponseInterceptor.swift
+++ Data/Api/LunchHubApi/ResponseInterceptor.swift
@@ -9,9 +9,32 @@
 import Foundation
 import RxSwift
 import Moya
+import SwiftExtensions
 
 protocol ResponseInterceptor {
     func intercept<T>(response: Response) -> Observable<T>? where T: Decodable
     func intercept(response: Response) -> Observable<Void>?
 }
 
+struct SuccessResponseInterceptor: ResponseInterceptor {
+    
+    func intercept<T>(response: Response) -> Observable<T>? where T: Decodable {
+        switch response.statusCode {
+        case 200...226:
+            do {
+                let decoded = try response.map(T.self, using: .snakeCaseDecoder)
+                return .just(decoded)
+            } catch {
+                return .error(LunchHubApi.Error(message: error.localizedDescription, code: 1))
+            }
+        default: return nil
+        }
+    }
+    
+    func intercept(response: Response) -> Observable<Void>? {
+        switch response.statusCode {
+        case 200...226: return .just(())
+        default: return nil
+        }
+    }
+}
