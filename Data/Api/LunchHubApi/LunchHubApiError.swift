f39c386fe5f81eb925d5e636bbcb321d8c951422
atsushi130 <atsushi_main@outlook.jp>
2019-04-30 22:15:11 +0900

    add: LunchHubApi.Error

---
+++ Data/Api/LunchHubApi/LunchHubApiError.swift
@@ -0,0 +1,16 @@
+//
+//  LunchHubApiError.swift
+//  Data
+//
+//  Created by Atsushi Miyake on 2019/04/30.
+//  Copyright © 2019 Tokyo Lunch Hub. All rights reserved.
+//
+
+import Foundation
+
+public extension LunchHubApi {
+    struct Error: Swift.Error, Decodable {
+        public let message: String
+        public let code: Int?
+    }
+}
