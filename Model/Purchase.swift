05dc16ed957d4ef39392f38135f9a4b5ffd1a4b0
atsushi130 <atsushi_main@outlook.jp>
2019-05-01 23:38:50 +0900

    define: Purchase model

---
+++ Model/Purchase.swift
@@ -0,0 +1,24 @@
+//
+//  Purchase.swift
+//  Model
+//
+//  Created by Atsushi Miyake on 2019/05/01.
+//  Copyright © 2019 Tokyo Lunch Hub. All rights reserved.
+//
+
+import Foundation
+import Extension
+import SwiftExtensions
+
+public struct Purchase: Decodable {
+    public let id: String
+    public let userId: String
+    public let status: Status
+    public let preparedAt: StringDecodableBox<Date>
+}
+
+public extension Purchase {
+    enum Status: Int, Decodable {
+        case created
+    }
+}
