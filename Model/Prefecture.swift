1a846ca77bd9131e938d735e287a3f83243f1a41
atsushi130 <atsushi_main@outlook.jp>
2019-05-01 23:43:06 +0900

    add: Prefecture model

---
+++ Model/Prefecture.swift
@@ -0,0 +1,14 @@
+//
+//  Prefecture.swift
+//  Model
+//
+//  Created by Atsushi Miyake on 2019/05/01.
+//  Copyright © 2019 Tokyo Lunch Hub. All rights reserved.
+//
+
+import Foundation
+
+public struct Prefecture: Decodable {
+    public let id: Int
+    public let name: String
+}
