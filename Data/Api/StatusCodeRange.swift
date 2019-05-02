aecf900577b1fd70b17128b7f41378997350a909
atsushi130 <atsushi_main@outlook.jp>
2019-05-03 00:27:13 +0900

    add: StatusCodeRange

--- Data/Api/StatusCodeRange.swift
+++ Data/Api/StatusCodeRange.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+enum StatusCodeRange: RawRepresentable {
+    
+    case information
+    case success
+    case redirection
+    case clientError
+    case serverError
+    case error
+    
+    var rawValue: ClosedRange<Int> {
+        switch self {
+        case .information:
+            return 100...103
+        case .success:
+            return 200...226
+        case .redirection:
+            return 300...308
+        case.clientError:
+            return 400...451
+        case .serverError:
+            return 500...511
+        case .error:
+            return 200...511
+        }
+    }
+    
+    init?(rawValue: ClosedRange<Int>) {
+        switch rawValue {
+        case 100...103:
+            self = .information
+        case 200...226:
+            self = .success
+        case 300...308:
+            self = .redirection
+        case 400...451:
+            self = .clientError
+        case 500...511:
+            self = .serverError
+        case 200...511:
+            self = .error
+        default:
+            return nil
+        }
+    }
+    
+    init(statusCode: Int) {
+        switch statusCode {
+        case 100...103:
+            self = .information
+        case 200...226:
+            self = .success
+        case 300...308:
+            self = .redirection
+        case 400...451:
+            self = .clientError
+        case 500...511:
+            self = .serverError
+        case 200...511:
+            self = .error
+        default:
+            // 不正なステータスコード
+            preconditionFailure()
+        }
+    }
+}
