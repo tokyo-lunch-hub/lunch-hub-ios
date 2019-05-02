bcf816072bf8cdce88f941cf5429cb48d3dd52c5
atsushi130 <atsushi_main@outlook.jp>
2019-05-02 23:43:36 +0900

    add: SuccessResponseInterceptor.swift

--- Data/Api/LunchHubApi/Interceptor/ResponseInterceptor.swift
+++ Data/Api/LunchHubApi/Interceptor/ResponseInterceptor.swift
@@ -29,29 +29,6 @@ protocol ResponseInterceptorable {
     func intercept(response: Response) -> Observable<Void>?
 }
 
-struct SuccessResponseInterceptor: ResponseInterceptorable {
-    
-    func intercept<T>(response: Response) -> Observable<T>? where T: Decodable {
-        switch response.statusCode {
-        case 200...226:
-            do {
-                let decoded = try response.map(T.self, using: .snakeCaseDecoder)
-                return .just(decoded)
-            } catch {
-                return .error(LunchHubApi.Error(message: error.localizedDescription, code: 1))
-            }
-        default: return nil
-        }
-    }
-    
-    func intercept(response: Response) -> Observable<Void>? {
-        switch response.statusCode {
-        case 200...226: return .just(())
-        default: return nil
-        }
-    }
-}
-
 struct FailureResponseInterceptor: ResponseInterceptorable {
     
     func intercept<T>(response: Response) -> Observable<T>? where T: Decodable {
